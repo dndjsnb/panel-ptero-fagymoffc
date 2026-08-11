922864b3b2c04b41833314b03e49ce256a5c2848
dndjsnb <faaahhmmii@gmail.com>
2026-08-11 19:03:47 +0700

    Add files via upload

--- ptero-api.js
+++ ptero-api.js
@@ -4,7 +4,7 @@ require('dotenv').config(); // Tetap dipanggil buat test lokal
 const pteroClient = axios.create({
     baseURL: `${process.env.PTERO_URL}/api/application`,
     headers: {
-        'Authorization': `Bearer ${process.env.PTERO_APP_KEY}`,
+        'Authorization': `Bearer ${process.env.PTERO_PTLA_KEY}`, // Pakai kunci admin (PLTA)
         'Content-Type': 'application/json',
         'Accept': 'Application/vnd.pterodactyl.v1+json'
     }
@@ -29,16 +29,20 @@ async function createUser(email, username) {
 async function createServer(userId, planData) {
     try {
         const res = await pteroClient.post('/servers', {
-            name: planData.name,
+            name: planData.name || `Bot-WA-${userId}`,
             user: userId,
-            egg: planData.egg_id,
-            docker_image: planData.docker_image,
-            startup: planData.startup,
-            environment: { SERVER_JARFILE: "server.jar" },
+            egg: 15, // Udah di-fix ke Egg 15 (Node.js / WA Bot)
+            docker_image: "ghcr.io/pterodactyl/yolks:nodejs_18", // Sesuaikan kalau lu pakai image lain
+            startup: "/usr/local/bin/node /home/container/index.js",
+            environment: { 
+                MAIN_FILE: "index.js",
+                AUTO_UPDATE: "0",
+                USER_UPLOAD: "0"
+            },
             limits: planData.limits,
             feature_limits: planData.feature_limits,
             deploy: {
-                locations: [1], // Ganti dengan ID Location Ptero lu
+                locations: [1], // Udah di-fix ke Location ID 1
                 dedicated_ip: false,
                 port_range: []
             }
@@ -51,4 +55,3 @@ async function createServer(userId, planData) {
 }
 
 module.exports = { createUser, createServer };
-
