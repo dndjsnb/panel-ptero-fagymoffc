22d085e74d2463b90cca6c7966e0fe0aad3c01db
dndjsnb <faaahhmmii@gmail.com>
2026-08-15 03:42:19 +0700

    Refactor webhook.js for improved functionality

--- api/webhook.js
+++ api/webhook.js
@@ -16,7 +16,7 @@ try {
 // ==========================================
 // MODE DEMO: Ubah jadi false kalau web sudah mau dirilis ke pembeli!
 // ==========================================
-const DEMO_MODE = false; 
+const DEMO_MODE = true; 
 
 const plans = {
     "basic": { ram: 1024, disk: 5000, cpu: 100, name: "Paket 1GB Basic", price: "5.000" },
